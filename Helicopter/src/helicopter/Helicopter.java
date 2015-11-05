47fc13d387fec24fa33e12069329ebad59024e84
ebh5067 <ebh5067@206-L09.up.ist.local>
2015-11-04 20:30:17 -0500

    sa

--- Helicopter/src/helicopter/Helicopter.java
+++ Helicopter/src/helicopter/Helicopter.java
@@ -15,7 +15,7 @@ public class Helicopter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Dont get Matt Whited!");
     }
     
 }
