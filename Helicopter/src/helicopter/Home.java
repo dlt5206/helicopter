95d6673a1782aa214ba70b6b060551f5ac304012
dlt5206 <dlt5206@206-L11.up.ist.local>
2015-11-04 20:45:51 -0500

    initialization

---
+++ Helicopter/src/helicopter/Home.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helicopter;
+
+import javax.swing.*;
+/**
+ *
+ * @author dlt5206
+ */
+public class Home extends JPanel {
+    
+    private JButton start = new JButton("Start");
+    private final static int FRAME_HEIGHT = 800;
+    private final static int FRAME_WIDTH = 800;
+    
+    
+    public Home() {
+        setLayout(null);
+        setSize(FRAME_HEIGHT, FRAME_WIDTH);
+        
+        
+    }
+}
